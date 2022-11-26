443426af2cb7d59f0fae343ff041c148579f1328
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-11-26 22:39:03 +0000

    Creating Routes

--- quiz_app/client/src/components/App.js
+++ quiz_app/client/src/components/App.js
@@ -1,10 +1,29 @@
 import "../styles/App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Main";
+import Quiz from "./Quiz";
+import Results from "./Results";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz></Quiz>,
+  },
+  {
+    path: "/result",
+    element: <Results></Results>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <h1> Heading</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
