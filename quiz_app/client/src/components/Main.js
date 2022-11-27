ed302583825bf7da364f0b55036c96af9215b0e2
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-11-27 19:49:36 +0000

    Main Component

--- quiz_app/client/src/components/Main.js
+++ quiz_app/client/src/components/Main.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import "../styles/main.css";
 
 function Main() {
-  return <div>Main</div>;
+  const inputRef = useRef(null);
+  return (
+    <div className="container">
+      <h1 className="title"> Quiz Application</h1>
+      <ol>
+        <li> You will be asked 10 question one after another!</li>
+        <li>You will be awarded 10 points for each correct answer</li>
+
+        <li>
+          For each question you will be presented with three options and you can
+          only select one
+        </li>
+
+        <li>
+          You will be able to review and change your answers before the end of
+          the quiz.
+        </li>
+        <li>The Results will be declared at the end of the quiz</li>
+      </ol>
+      <form id="form">
+        <input
+          ref={inputRef}
+          className="userid"
+          type="text"
+          placeholder="Username*"
+        />
+      </form>
+
+      <div className="start">
+        <Link className="btn" to={"quiz"}>
+          Start Quiz
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
