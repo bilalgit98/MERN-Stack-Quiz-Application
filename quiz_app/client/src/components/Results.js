e5a0b271ee710a3a04ea4926443c87957397cd81
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-11-28 15:48:10 +0000

    Results Component

--- quiz_app/client/src/components/Results.js
+++ quiz_app/client/src/components/Results.js
@@ -1,7 +1,52 @@
 import React from "react";
-
+import "../styles/Result.css";
+import { Link } from "react-router-dom";
+import ResultTable from "./ResultTable";
 function Results() {
-  return <div>Results</div>;
+  function onRestart() {
+    console.log("onrestart");
+  }
+  return (
+    <div className="container">
+      <h1 className="title text-light">Quiz Application </h1>
+      <div className="result flex-center">
+        <div className="flex">
+          <span>Username </span>
+          <span className="bold">ff</span>
+        </div>
+
+        <div className="flex">
+          <span>Total Quiz Points : </span>
+          <span className="bold">50</span>
+        </div>
+
+        <div className="flex">
+          <span>Total Questions : </span>
+          <span className="bold">50</span>
+        </div>
+
+        <div className="flex">
+          <span>Total Earnt Points : </span>
+          <span className="bold">50</span>
+        </div>
+
+        <div className="flex">
+          <span>Quiz Result : </span>
+          <span className="bold">fail</span>
+        </div>
+      </div>
+
+      <div className="start">
+        <Link className="btn" to={"/"} onClick={onRestart}>
+          Restart
+        </Link>
+      </div>
+
+      <div className="container ">
+        <ResultTable />
+      </div>
+    </div>
+  );
 }
 
 export default Results;
