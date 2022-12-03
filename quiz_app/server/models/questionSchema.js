dac579d92ad71341898a94f161dd016bb2690986
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-03 22:37:50 +0000

    Models

---
+++ quiz_app/server/models/questionSchema.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose";
+const { Schema } = mongoose;
+
+//question model
+const questionModel = new Schema({
+  questions: { type: Array, default: [] },
+  answers: { type: Array, default: [] },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// "Questions" is the name of the model
+//"questionModel" is the schema
+export default mongoose.model("Questions", questionModel);
