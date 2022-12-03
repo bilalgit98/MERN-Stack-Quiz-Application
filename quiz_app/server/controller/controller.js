274be1fe82c2c726ebb326d6e9497939318c0553
bilalgit98 <71947506+bilalgit98@users.noreply.github.com>
2022-12-03 17:43:46 +0000

    Controller

---
+++ quiz_app/server/controller/controller.js
@@ -0,0 +1,22 @@
+export async function getQuestions(req, res) {
+  res.json("Questions Request");
+}
+
+export async function insertQuestions(req, res) {
+  res.json("Questions Api Post Requests");
+}
+
+export async function deleteQuestions(req, res) {
+  res.json("Questions Api delete Requests");
+}
+
+export async function getResult(req, res) {
+  res.json("result api get Requests");
+}
+export async function postResult(req, res) {
+  res.json("result api post Requests");
+}
+
+export async function deleteResult(req, res) {
+  res.json("result api delete Requests");
+}
--- quiz_app/server/router/route.js
+++ quiz_app/server/router/route.js
@@ -1,13 +1,20 @@
 import { Router } from "express";
 const router = Router();
+import * as controller from "../controller/controller.js";
+
 //Questions Route
+// router.get("/questions", controller.getQuestions);
+// router.post("/questions", controller.insertQuestions);
 
-router.get("/questions", (req, res) => {
-  try {
-    res.json("Questions Request");
-  } catch (error) {
-    res.json(error);
-  }
-});
+router
+  .route("/questions")
+  .get(controller.getQuestions)
+  .post(controller.insertQuestions)
+  .delete(controller.deleteQuestions);
 
+router
+  .result("/result")
+  .get(controller.getResult)
+  .post(controller.postResult)
+  .delete(controller.deleteResult);
 export default router;
